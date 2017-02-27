61114e2974a6f4494ecdf970a409335413ba25f0
Hannah Schuster <hschuster9@gmail.com>
2017-02-27 11:49:11 -0500

    comments functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,41 @@
 class CommentsController < ApplicationController
+
+  def new
+    @event = Event.find(params[:event_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @comment = @event.comments.create(comment_params)
+    # @comment = current_user.comments.create!(comment_params)
+    redirect_to event_path(@event)
+  end
+
+  def edit
+    @event = Event.find(params[:event_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:event_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    @event = Event.find(params[:event_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to event_path(@event)
+
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment_by, :content)
+  end
 end
