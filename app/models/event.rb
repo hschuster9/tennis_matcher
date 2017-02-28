dd313d320527680226886ea6066e0a78901ffe22
Hannah Schuster <hschuster9@gmail.com>
2017-02-28 18:05:25 -0500

    search function

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
+  def self.search(search)
+    where("title ILIKE ? OR location ILIKE ? OR description ILIKE ? OR posted_by ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%","%#{search}%")
+  end
 end
