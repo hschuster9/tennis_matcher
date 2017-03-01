d57aaf24b0b43d65eff2a47c23ed7bb5c253a151
Hannah Schuster <hschuster9@gmail.com>
2017-03-01 10:48:27 -0500

    add optional true to user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :event
-  
-  belongs_to :user
+
+  belongs_to :user, optional: true
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
-  belongs_to :user
+  belongs_to :user, optional: true
   def self.search(search)
     where("title ILIKE ? OR location ILIKE ? OR description ILIKE ? OR posted_by ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%","%#{search}%")
   end
